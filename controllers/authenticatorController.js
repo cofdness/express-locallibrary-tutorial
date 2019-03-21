f4a08bbfd9b867f0ca0d5337e03912bc79297023
Phung Vu Tran Kim <vtkp2002dn@gmail.com>
2019-03-21 11:16:11 +0700

    Login page

--- controllers/authenticatorController.js
+++ controllers/authenticatorController.js
@@ -1,13 +1,37 @@
 const User = require('../models/user')
-const {body, validationResult} = require('express-validator/check')
+const {check, body, validationResult} = require('express-validator/check')
 const {sanitizeBody} = require('express-validator/filter')
+const bcrypt = require('bcrypt')
 
 exports.login_get = (req, res, next) => {
-    res.send('will implement')
+    res.render('login_form', {title:'Login'})
 }
-exports.login_post = (req, res, next) => {
-    res.send('will implement')
-}
+exports.login_post = [
+    check('email').isEmail().normalizeEmail(),
+    check('password').isLength({min: 1}).trim().escape(),
+    (req, res, next) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.render('login_form',{title:'Login', user: req.body, errors: errors.array()})
+        }
+        else {
+            User.authenticate(req.body.email, req.body.password, (error, user) => {
+                if (error || !user) {
+                    const err = new Error('Invalid email or password')
+                    err.status = 401
+                    err.msg = 'Invalid email or password'
+                    console.log(err)
+                    console.log(user)
+                    res.render('login_form', {title: 'Login', user: user, errors: [err]})
+                }
+                else {
+                    req.session.userId = user._id
+                    res.redirect('/user/profile')
+                }
+            })
+        }
+    }
+]
 exports.register_get = (req, res, next) => {
     res.render('register_form', {title:'Register'})
 }
@@ -15,7 +39,7 @@ exports.register_post = [
     (req, res, next) => {
         if  (req.body.password !== req.body.confirmpassword) {
             const err = new Error('Confirm password not match')
-            return res.render('register_form', {user: req.body, errors: [error]})
+            return res.render('register_form', {user: req.body, errors: [err]})
         }
         next()
     },
@@ -23,7 +47,6 @@ exports.register_post = [
     body('email').isEmail().normalizeEmail(),
     body('password').isLength({min: 5}).trim().escape(),
     (req, res, next) => {
-        console.log('abc')
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             return res.render('register_form', {user: req.body, errors: errors.array()})
@@ -34,8 +57,6 @@ exports.register_post = [
                 email: req.body.email,
                 password: req.body.password
             }
-            console.log(req.body.name)
-            console.log(req.body.password)
             User.create(userData, (err, user) => {
                 if (err) return next(err)
                 else {
@@ -47,7 +68,14 @@ exports.register_post = [
     }
 ]
 exports.logout_get = (req, res, next) => {
-    res.send('will implement')
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) return next(err)
+            else {
+                return res.redirect('/')
+            }
+        })
+    }
 }
 exports.logout_post = (req, res, next) => {
     res.send('will implement')
